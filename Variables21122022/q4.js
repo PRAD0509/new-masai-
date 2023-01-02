c15d4d919d91484a6a5508e7da5341a39eda5d99
PRAD0509 <praveendash0718@gmail.com>
2023-01-02 12:08:56 +0530

    variables done

---
+++ Variables21122022/q4.js
@@ -0,0 +1,79 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+
+
+let a= "=======================================================================";
+console.log(a);
+
+let mark_sheet = "                                MARK SHEET                   ";   
+console.log(mark_sheet);
+let b ="=======================================================================";
+console.log(b);
+
+let name_of_stud= "Name of Student :-        PRAVEEN PRADEEP DASH";
+console.log(name_of_stud);
+let c="-----------------------------------------------------------------------";
+console.log(c);
+
+let name_of_schl="Name of School  :-        S.E.S English School";
+console.log(name_of_schl);
+let d="-----------------------------------------------------------------------";
+console.log(d);
+
+let stand="Standard        :- 10TH                             Roll No  :- E062309";
+console.log(stand);
+let e="-----------------------------------------------------------------------";
+console.log(e);
+
+let d_o_b="Date of Birth   :- 05/03/1998";
+console.log(d_o_b);
+let f="=======================================================================";
+console.log(f);
+
+let sub="SUBJECT                    MARKS OBTAINED                   TOTAL MARKS";
+console.log(sub);
+let g="=======================================================================";
+console.log(g);
+
+let hindi="HINDI                            78                              100";
+console.log(hindi);
+let h="-----------------------------------------------------------------------";
+console.log(h);
+
+let marathi="MARATHI                          79                              100";
+console.log(marathi);
+let i="-----------------------------------------------------------------------";
+console.log(i);
+
+let eng="ENGLISH                          81                              100";
+console.log(eng);
+let j="-----------------------------------------------------------------------";
+console.log(j);
+
+let soc_stud="SOCIAL STUDIES                   85                              100";
+console.log(soc_stud);
+let k="-----------------------------------------------------------------------";
+console.log(k);
+
+let sci="SCIENCE                          82                              100";
+console.log(sci);
+let l="-----------------------------------------------------------------------";
+console.log(l);
+
+let math="MATHEMATICS                      65                              100";
+console.log(math);
+let m="=======================================================================";
+console.log(m);
+
+let total="TOTAL MARKS                     470                             600";
+console.log(total);
+let n="=======================================================================";
+console.log(n);
+
+let grade="GRADE :- 'A '                                     PERCENTAGE :- 78.34  ";
+console.log(grade);
+let o=
+"=======================================================================";
+console.log(o);
+
